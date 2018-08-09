bb3d63be5041fe486b7943331031ea7e9d21f752
af4ro <1997anshul@gmail.com>
2018-08-09 13:37:52 -0700

    Added blueprints

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,20 +1,23 @@
 # services/users/manage.py
 
 
-from flask.cli import FlaskGroup
-
-from project import app, db  # new
 import unittest
 
-cli = FlaskGroup(app)
+from flask.cli import FlaskGroup
+
+from project import create_app, db   # new
+from project.api.models import User  # new
+
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 
 
-# new
 @cli.command()
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
 
 @cli.command()
 def test():
@@ -24,6 +27,7 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
 
 if __name__ == '__main__':
     cli()
